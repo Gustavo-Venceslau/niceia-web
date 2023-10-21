2df55a8fc40eea00214b2ad1eb5f6aa14780d8a4
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-10-21 18:18:31 -0300

    message component initialized

--- app/chat/components/chatContent/messagesContent/messagesContent.tsx
+++ app/chat/components/chatContent/messagesContent/messagesContent.tsx
@@ -1,8 +1,12 @@
+import { Message } from './components/message';
 import { MessagesInput } from './components/messageInput';
 
 export function MessagesContent() {
 	return (
-		<div className="w-full h-full flex flex-col justify-end">
+		<div className="w-full h-full flex flex-col justify-end relative overflow-hidden gap-4">
+			<div className="w-full h-full flex flex-col justify-end gap-4">
+				<Message message='OMG 😲 do you remember what you did last night at the work night out?'/>
+			</div>
 			<MessagesInput />
 		</div>
 	)
--- app/chat/components/messagesNavbar/chatList/components/chatBox.tsx
+++ app/chat/components/messagesNavbar/chatList/components/chatBox.tsx
@@ -1,6 +1,15 @@
+"use client"
+
+import { useNavbarContext } from "../../navbar/contexts/navbarContext"
+
 export function ChatBox(){
+
+	const { isVisible } = useNavbarContext();
+
+	const chatBoxDisplay = isVisible ? "flex" : "hidden";
+
 	return (
-		<button className="w-full flex gap-4 px-4 py-3 items-center hover:bg-[#F5F5F5]">
+		<button className={`w-full ${chatBoxDisplay} gap-4 px-4 py-3 items-center hover:bg-[#F5F5F5]`}>
 			<div className="min-w-[48px] h-12 bg-[#8BABD8] rounded-full"></div>
 			<div className="w-full flex flex-col items-center">
 				<div className="w-full flex items-center justify-between">
